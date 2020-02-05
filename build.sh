5b183a56187b0e4ebb4e953cd167ddf372d241bc
Tom Yandell <tom@tomyandell.co>
2020-02-05 16:21:27 +0000

    Build binaries

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+version=$1
+if [ -z "$version" ]; then
+    echo version is required >&2
+    exit 1
+fi
+
+rm -f cdflow2*
+
+git archive --format=tar.gz --prefix=cdflow2-$version/ --output=cdflow2-$version.tar.gz $version
+
+GOOS=linux GOARCH=amd64 go build -o cdflow2-linux-amd64 .
+GOOS=darwin GOARCH=amd64 go build -o cdflow2-darwin-amd64 .
+GOOS=windows GOARCH=amd64 go build -o cdflow2-windows-amd64 .
