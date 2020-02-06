1fd56cf470bb733f5ecf75eddb000145411131eb
Tom Yandell <tom@tomyandell.co>
2020-02-06 12:18:26 +0000

    Set the version number during build

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,6 +12,6 @@ rm -f cdflow2*
 
 git archive --format=tar.gz --prefix=cdflow2-$version/ --output=cdflow2-$version.tar.gz $version
 
-GOOS=linux GOARCH=amd64 go build -o cdflow2-linux-amd64 .
-GOOS=darwin GOARCH=amd64 go build -o cdflow2-darwin-amd64 .
-GOOS=windows GOARCH=amd64 go build -o cdflow2-windows-amd64 .
+GOOS=linux GOARCH=amd64 go build -o cdflow2-linux-amd64 -ldflags="-X main.version=$version" .
+GOOS=darwin GOARCH=amd64 go build -o cdflow2-darwin-amd64 -ldflags="-X main.version=$version" .
+GOOS=windows GOARCH=amd64 go build -o cdflow2-windows-amd64 -ldflags="-X main.version=$version" .
