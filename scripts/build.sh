f8ab434d2e946a48d37783cf6259f452f4f5fb10
Keir Badger <keir.badger@acuris.com>
2021-07-09 14:19:01 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,4 +15,3 @@ git archive --format=tar.gz --prefix=cdflow2-$version/ --output=cdflow2-$version
 GOOS=linux GOARCH=amd64 go build -o cdflow2-linux-amd64 -ldflags="-X main.version=$version" .
 GOOS=darwin GOARCH=amd64 go build -o cdflow2-darwin-amd64 -ldflags="-X main.version=$version" .
 GOOS=windows GOARCH=amd64 go build -o cdflow2-windows-amd64 -ldflags="-X main.version=$version" .
-GOOS=linux GOARCH=arm64 go build -o cdflow2-linux-arm64 -ldflags="-X main.version=$version" .
