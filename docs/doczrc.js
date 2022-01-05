f6cd6452e1e4efedd2339700a29b046e381f79dc
Osh Slomczynski <8842282+oshdev@users.noreply.github.com>
2022-01-05 16:21:38 +0000

    Create doczrc.js

---
+++ docs/doczrc.js
@@ -0,0 +1,21 @@
+export default {
+  title: 'cdflow2',
+  description: 'deployment tooling for continuous delivery',
+  base: '/opensource/cdflow2',
+  menu: [
+    'Overview',
+    'Installation',
+    'Project Setup',
+    { name: 'Commands', menu: [
+      'Usage',
+      'Setup',
+      'Release',
+      'Deploy',
+      'Destroy',
+      'Common Terraform Setup'
+    ] },
+    'cdflow.yaml Reference',
+  ],
+  host: '0.0.0.0',
+  src: './src'
+}
