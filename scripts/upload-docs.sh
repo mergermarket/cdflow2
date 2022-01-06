b8ce404e9bfebe7037b59cc4c90593c890a99e5f
Keir Badger <keir.badger@acuris.com>
2022-01-06 15:45:08 +0000

    more debug code

--- scripts/upload-docs.sh
+++ scripts/upload-docs.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-set -e
-
 if [ "$1" == "live" ]; then
     fastly_site_id=0D4hN8fWNcMnEp6CbAIDup
     bucket=live-developer-frontend-router-static-content
+    fromAwsAccountAlias=mmgprod
 elif [ "$1" == "aslive" ]; then
     fastly_site_id=08pLXpbfh088YBYI1c4Qlk
     bucket=aslive-developer-frontend-router-static-content
+    fromAwsAccountAlias=mmgdev
 else
     echo "environment parameter is required - aslive or live" >&2
     exit 1
 fi
 
+echo ${fromAwsAccountAlias}
+
+docker pull mergermarket/switch-role
+SAVE_XTRACE_STATUS="\$(set +o | grep xtrace)"; set +x
+eval "$( docker run --rm -i \
+    -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN  \
+    mergermarket/switch-role \
+        --account "$fromAwsAccountAlias" \
+        --role-name admin \
+        --role-session-name "$JOB_NAME-upload" \
+)"
+eval "$SAVE_XTRACE_STATUS"; unset SAVE_XTRACE_STATUS
+
+echo "help"
+aws help
+
+echo "call identity"
 aws sts get-caller-identity
 
+echo "sync"
 aws s3 sync --delete --metadata 'Surrogate-Key=cdflow2' docs/dist/ s3://$bucket/opensource/cdflow2/
 
 if [ "$FASTLY_API_KEY" != "" ]; then
