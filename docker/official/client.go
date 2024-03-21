0edddf03c5c01be41651822a20aa5c4f0876e8b9
Marcio Goda <marciogoda@gmail.com>
2024-03-21 14:52:05 +0000

    changed stop options timeout to int

--- docker/official/client.go
+++ docker/official/client.go
@@ -9,7 +9,6 @@ import (
 	"io"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types"
@@ -397,13 +396,13 @@ func (dockerClient *Client) Exec(options *docker.ExecOptions) error {
 }
 
 // Stop stops a container.
-func (dockerClient *Client) Stop(id string, timeout time.Duration) error {
-	return dockerClient.client.ContainerStop(context.Background(), id, &timeout)
+func (dockerClient *Client) Stop(id string, timeout int) error {
+	return dockerClient.client.ContainerStop(context.Background(), id, container.StopOptions{Timeout: &timeout})
 }
 
 // CreateVolume creates a docker volume and returns its ID.
 func (dockerClient *Client) CreateVolume(name string) (string, error) {
-	volume, err := dockerClient.client.VolumeCreate(context.Background(), volume.VolumeCreateBody{
+	volume, err := dockerClient.client.VolumeCreate(context.Background(), volume.CreateOptions{
 		Name: name,
 	})
 	if err != nil {
